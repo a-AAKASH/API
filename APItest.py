fd00f134a2190632188b00d8a506ac1ecbe6c609
a-AAKASH <aakashpaudyal@gmail.com>
2022-06-29 01:52:28 +0100

    changes to code

--- APItest.py
+++ APItest.py
@@ -8,12 +8,15 @@ url = "https://api.uptimerobot.com/v2/getMonitors"
 
 payload = {"api_key" : "ur828173-4544bcf8143509466865be09",
             "monitors" : "790608131-784355743-784355767-784355758-784355759",
-            "log_types" : "1", 
+            "log_types" : "1-99-98", 
             #"logs_start_date" : "1655679600",
             #"logs_end_date" : "1656284400",
-            "custom_uptime_ranges" : "1655679600_1656284400",
+            #"custom_uptime_ratios" : "7",
+            #"custom_uptime_ranges" : "1655679600_1656284400",
+            "logs_limit" : "1",
             "logs" : "1"
-            }
+            #"statuses" : "2"
+            } 
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
@@ -21,9 +24,16 @@ headers = {
 
 response = requests.request("POST", url, data=payload, headers=headers)
           
-prettyjson = json.loads(response.text)
-print(json.dumps(prettyjson, indent=1))
+#prettyjson = json.loads(response.text)
+#print(json.dumps(prettyjson, indent=1))
 
+#print(response.json())
+
+#jsonFile = open("data.json", "w")
+#jsonFile.write(json.dumps(prettyjson, indent=1))
+#jsonFile.close()
+
+jsonString = json.dumps(response.json(), indent=1)
 jsonFile = open("data.json", "w")
-jsonFile.write(json.dumps(prettyjson))
+jsonFile.write(jsonString)
 jsonFile.close()
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ API DOCS: https://uptimerobot.com/api/
 
 ## Problem at hand:
 
-• Provide the average uptime for the last week <br />
-• Provide the details of the last downtime <br />
-• Calculate the average total uptime across all monitors since the last downtime event <br />
+1. Provide the average uptime for the last week <br />
+2. Provide the details of the last downtime <br />
+3. Calculate the average total uptime across all monitors since the last downtime event <br />
 
 Reference information: <br />
 • API key (ur828173-4544bcf8143509466865be09) <br />
@@ -14,3 +14,10 @@ Reference information: <br />
 • Documentation (https://uptimerobot.com/api/) <br />
 • List of monitors (790608131, 784355743, 784355767, 784355758, and 784355759) <br />
 
+## Answers
+
+Calculation for Average uptime for the last week:    [ Weekly downtime periods(in seconds) = 604800 - (Uptime * 604800) ]
+
+Monitor : *131
+    Duration : 11951720 (i.e. downtime in seconds)
+    
